1debc77c186298b5ae4d016e6f26a16275ee3ce6
zhangyh199475 <zhangyh_upc@qq.com>
2020-04-25 21:27:00 +0800

    update

---
+++ src/test/java/com/ibm/sba/SbaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.sba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
